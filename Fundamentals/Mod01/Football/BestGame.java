89e762d0b86255db31c852da76c2f201a10c5b3f
Dana Wyatt <danaatemca@aol.com>
2021-10-18 14:35:29 -0500

    Cowboyse beat Patriots

---
+++ Fundamentals/Mod01/Football/BestGame.java
@@ -0,0 +1,12 @@
+public class BestGame {
+    public static void main(String[] args) {
+        String team1Name = "Dallas Cowboys";
+        int score1 = 35;
+
+        String team2Name = "NE Patriots";
+        int score2 = 29;
+
+        String gameInfo = team1Name + " " + score1 + " * " + score2 + " " + team2Name;
+        System.out.println(gameInfo);
+    }
+}
