f71d63ac80f94c5154daff6795077bc6df266d5e
Dana Wyatt <danaatemca@aol.com>
2021-10-18 15:51:52 -0500

    mixed operand types in formulas

---
+++ Fundamentals/Mod01/MathPlay/MathPlay.java
@@ -0,0 +1,19 @@
+
+public class MathPlay {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 3;
+
+        double answer1 = num1 / num2 * 1.0f;
+        float answer2 = 1.0f * num1 / num2;
+        float answer3 = num1 * 1.0f / num2;
+        System.out.println(answer1);
+        System.out.println(answer2);
+        System.out.println(answer3);
+
+        long x = 1;
+        
+    }
+}
+
+
