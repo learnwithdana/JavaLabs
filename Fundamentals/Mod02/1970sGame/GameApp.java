9f6311e955f663145dcaf0436666fc7b6d250f81
Dana Wyatt <danaatemca@aol.com>
2021-10-19 13:28:25 -0500

    Finished 1970sGameApp

--- Fundamentals/Mod02/1970sGame/GameApp.java
+++ Fundamentals/Mod02/1970sGame/GameApp.java
@@ -19,16 +19,27 @@ public class GameApp {
             System.out.println("Howdy " + name + "!");
         }
         else if (command == 2) {
-
+            System.out.print("Wnat is your name? ");
+            String name = scanner.nextLine();
+            System.out.println("Goodbye " + name + "!");
         }
         else if (command == 3) {
-
+            System.out.print("What number do you want to know the square root of? ");
+            double number = scanner.nextDouble();
+            double answer = Math.sqrt(number);
+            System.out.println("The square root of " + number + " is " + answer);            
+            System.out.printf("The square root of %f is %.4f\n", number, answer);
         }
         else if (command == 4) {
-
+            System.out.print("Enter the base: ");
+            double base = scanner.nextDouble();
+            System.out.print("Enter the exponent: ");
+            double exp = scanner.nextDouble();
+            double answer = Math.pow(base, exp);
+            System.out.printf("%f to the power of %f is %f\n", base, exp, answer);
         }
         else {
-
+            System.out.println("ERROR:  Commands numbers must be between 1 and 4!");
         }
     }
 }
